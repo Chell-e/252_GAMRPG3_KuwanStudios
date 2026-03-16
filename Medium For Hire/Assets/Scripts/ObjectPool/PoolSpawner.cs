1f5868d85052701ab2eb1c6bc75e563919ff2be5
Chelle-e <michelleandrea.manabat@benilde.edu.ph>
2026-03-16 12:34:11 +0800

    Deleted unnecessary files and assets

--- Medium For Hire/Assets/Scripts/ObjectPool/PoolSpawner.cs
+++ Medium For Hire/Assets/Scripts/ObjectPool/PoolSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestPoolSpawner : MonoBehaviour
+public class PoolSpawner : MonoBehaviour
 {
     [System.Serializable]
     public class Wave
