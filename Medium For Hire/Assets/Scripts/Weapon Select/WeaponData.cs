b28029aca8a2a3508f12164bc1d2a0cca4e8999e
DiegoEnricoJimenez-CSB <163430925+DiegoEnricoJimenez-CSB@users.noreply.github.com>
2026-07-27 19:32:47 +0800

    slight functionality for weapon selection
    - added name, description, behavior text fields for WeaponData scriptable object
    - WeaponSelectionManager loads the data into UI
    - added more to TMPro sprite sheet

--- Medium For Hire/Assets/Scripts/Weapon Select/WeaponData.cs
+++ Medium For Hire/Assets/Scripts/Weapon Select/WeaponData.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "WeaponSelect/Weapon Data")]
 public class WeaponData : ScriptableObject
 {
-    public string weaponName;
+    [Header("REFERENCES")]
     public GameObject weaponPrefab;
     public WeaponUnlock mainWeaponUIData;
     // sprite?
 
+    [Header("UI")]
+    [TextArea(2, 2)]
+    public string weaponTitle;
+    [Space]
+    [TextArea(5,7)]
+    public string weaponDescription;
+    [Space]
+    [TextArea(5, 7)]
+    public string normalBehavior;
+    [Space]
+    [TextArea(5, 7)]
+    public string aimedBehavior;
     // auto, aimed, evolutions
 }
--- Medium For Hire/Assets/Scripts/Weapon Select/WeaponSelectManager.cs
+++ Medium For Hire/Assets/Scripts/Weapon Select/WeaponSelectManager.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class WeaponSelectManager : MonoBehaviour
 {
+    [Header("REFERENCES")]
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI descriptionText;
+
+    [SerializeField] TextMeshProUGUI normalText;
+    [SerializeField] TextMeshProUGUI aimedText;
+
     public void SelectWeapon(WeaponData selectedWeaponData)
     {
         if (selectedWeaponData == null) return;
 
         // pass the selected weapon data to the stage manager
         StageManager.SelectedWeapon = selectedWeaponData;
-        Debug.Log("Selected Weapon: " + selectedWeaponData.weaponName);
+        Debug.Log("Selected Weapon: " + selectedWeaponData.weaponTitle);
+
+        // load the UI
+        LoadInfo(selectedWeaponData);
+    }
+
+    private void LoadInfo(WeaponData selectedWeaponData)
+    {
+        nameText.text = selectedWeaponData.weaponTitle;
+        descriptionText.text = selectedWeaponData.weaponDescription;
+        normalText.text = selectedWeaponData.normalBehavior;
+        aimedText.text = selectedWeaponData.aimedBehavior;
+
     }
 
     public void ConfirmSelectedWeapon()
